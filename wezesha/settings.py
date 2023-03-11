3af3325e16a61b1e99be793028587e6a244b2166
kagwe <kagwepeter07@gmail.com>
2023-03-11 19:31:43 +0300

    'fc'

--- wezesha/settings.py
+++ wezesha/settings.py
@@ -87,12 +87,25 @@ AUTH_USER_MODEL = 'wezacare.CustomUser'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': "django.db.backends.postgresql_psycopg2",
+        'HOST': "db.bwnowbyodlhpfdxfesoo.supabase.co",
+        'NAME': "postgres",
+        'USER': "postgres",
+        'PASSWORD': "@Wezacare2023",
+        'PORT': "5432",
+
     }
 }
+
 
 
 # Password validation
